7f089a6a9640ff7a96488d6ab2e8d2710fdbfb11
charinda <charinda04@gmail.com>
2019-02-01 17:14:14 +0530

    logo added

---
+++ src/components/index.js
@@ -0,0 +1,15 @@
+import Navbar from './Navbar';
+import Cart from './Cart';
+import Default from './Default';
+import Details from './Details';
+import Product from './Product';
+import ProductList from './ProductList';
+
+export {
+  Navbar,
+  Cart,
+  Default,
+  Details,
+  Product,
+  ProductList,
+};
