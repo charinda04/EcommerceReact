b19cedd718043589ffcd06a730e5e9dd89fb3b76
charinda <charinda04@gmail.com>
2019-02-02 19:37:38 +0530

    cart Item display completed

--- src/context.js
+++ src/context.js
@@ -9,9 +9,12 @@ class ProductProvider extends Component {
   state = {
     products: [],
     detailProduct: detailProduct,
-    cart: [],
+    cart: storeProducts,
     modalOpen: false,
-    modalProduct: detailProduct
+    modalProduct: detailProduct,
+    cartSubTotal: 0,
+    cartTax: 0,
+    cartTotal: 0
   };
 
   componentDidMount() {
@@ -77,6 +80,22 @@ class ProductProvider extends Component {
     });
   };
 
+  increment = id => {
+    console.log("this is the increment method");
+  };
+
+  decrement = id => {
+    console.log("this is the decrement method");
+  };
+
+  removeItem = id => {
+    console.log("this is the removeItem method");
+  };
+
+  clearCart = () => {
+    console.log("this is the clearCart method");
+  };
+
   render() {
     return (
       <ProductContext.Provider
@@ -85,7 +104,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal: this.openModal,
-          closeModal: this.closeModal
+          closeModal: this.closeModal,
+          increment: this.increment,
+          decrement: this.decrement,
+          removeItem: this.removeItem,
+          clearCart: this.clearCart
         }}
       >
         {" "}
