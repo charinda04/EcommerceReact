ad649dda29943b447462ebd3b6d5f4c6b23ccdc7
charinda <charinda04@gmail.com>
2019-02-02 15:47:22 +0530

    adding to the cart completed

--- src/context.js
+++ src/context.js
@@ -7,16 +7,53 @@ const ProductContext = React.createContext();
 
 class ProductProvider extends Component {
   state = {
-    products: storeProducts,
-    detailProduct: detailProduct
+    products: [],
+    detailProduct: detailProduct,
+    cart: []
   };
 
-  handleDetail = () => {
-    console.log("hello from detail");
+  componentDidMount() {
+    this.setProducts();
+  }
+
+  setProducts = () => {
+    let products = [];
+    storeProducts.forEach(item => {
+      const singleItem = {
+        ...item
+      };
+      products = [...products, singleItem];
+    });
+    this.setState(() => {
+      return {
+        products
+      };
+    });
   };
 
-  addToCart = () => {
-    console.log("hello from add to cart");
+  getItem = id => {
+    const product = this.state.products.find(item => item.id === id);
+    return product;
+  };
+
+  handleDetail = id => {
+    const product = this.getItem(id);
+    this.setState(() => {
+      return { detailProduct: product };
+    });
+  };
+
+  addToCart = id => {
+    let tempProducts = [...this.state.products];
+    const index = tempProducts.indexOf(this.getItem(id));
+    const product = tempProducts[index];
+    product.inCart = true;
+    product.count = 1;
+    const price = product.price;
+    product.total = price;
+    this.setState(() => {
+      return { products: tempProducts, cart: [...this.state.cart, product] };
+    });
   };
 
   render() {
@@ -28,7 +65,8 @@ class ProductProvider extends Component {
           addToCart: this.addToCart
         }}
       >
-        {this.props.children}
+        {" "}
+        {this.props.children}{" "}
       </ProductContext.Provider>
     );
   }
