c2c5838fecb03ac7b578be6abe3cc685956e8baf
charinda <charinda04@gmail.com>
2019-02-03 13:11:14 +0530

    WIP: cart total

--- src/components/cart/CartTotal.js
+++ src/components/cart/CartTotal.js
@@ -1,9 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-export default function CartTotal() {
+export default function CartTotal({ value }) {
+  const { cartSubtotal, cartTax, cartTotal, clearCart } = value;
   return (
-    <div>
-      <h1>this is the cart total</h1>
-    </div>
+    <React.Fragment>
+      <div className="container">
+        <div className="row">
+          <div className="col-10 mt-2 ml-sm-5 ml-md-auto col-sm-8 text-capitalize text-right">
+            <Link to="/">
+              <button>clear cart</button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
   );
 }
